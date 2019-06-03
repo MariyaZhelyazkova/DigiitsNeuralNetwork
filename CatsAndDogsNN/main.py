d342c08eb2f2275568f56f8aad66088243879134
ilia.karkamov <ilia.karkamov07@gmail.com>
2019-06-03 17:01:42 +0300

    Test image loading and showing was broken

--- CatsAndDogsNN/main.py
+++ CatsAndDogsNN/main.py
@@ -1,4 +1,3 @@
-import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Convolution2D
 from tensorflow.keras.layers import MaxPooling2D
@@ -6,6 +5,19 @@ from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from skimage import transform
+import os, os.path
+
+def load_image(filename):
+   return Image.open(filename)
+
+def prepare_image(image):
+   image = np.array(image).astype('float32') / 255
+   image = transform.resize(image, (128, 128, 3))
+   image = np.expand_dims(image, axis = 0)
+   return image
 
 classifier = Sequential()
 classifier.add(Convolution2D(64, 3, 3, input_shape = (128, 128, 3), activation = 'relu'))
@@ -35,23 +47,23 @@ validation_set = validation_datagen.flow_from_directory('dataset/test_set',
 
 classifier.fit_generator(training_set,
                          steps_per_epoch = 8000,
-                         epochs = 1,
+                         epochs = 3,
                          validation_data = validation_set,
                          validation_steps = 800,
-                         workers = 4)
+                         workers = 8)
 
-test_datagen = ImageDataGenerator(rescale = 1. / 255)
-test_set = test_datagen.flow_from_directory('dataset/test_images',
-                                            target_size = (128, 128),
-                                            batch_size = 64,
-                                            class_mode = 'binary')
 
-result = classifier.predict(test_set);
+test_images = []
+test_images_path = 'dataset/test_images'
 
-x, y = test_set.next();
+for f in os.listdir(test_images_path):
+    test_images.append(load_image(os.path.join(test_images_path, f)))    
 
-for i, img_pred in enumerate(result):          
-    plt.imshow(x[i])
-    plt.show()
-    print('Dog: %f' % (img_pred[0]))
-    print('Cat: %f' % (1 - img_pred[0]))
+for image in test_images:        
+    plt.imshow(image)
+    plt.show()    
+    
+    result = classifier.predict(prepare_image(image));    
+    
+    print('Dog: %f' % (result[0][0]))
+    print('Cat: %f' % (1 - result[0][0]))
